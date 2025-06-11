0f89621b4dc815af887cf517c9de664dd3879b89
lanzkyy <lanagoodest@gmail.com>
2025-06-12 00:00:44 +0800

    Update config/db.js with latest database configuration

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "roomease",
+  uri: process.env.DB_HOST,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
